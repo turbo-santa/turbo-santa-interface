f7b39200380962157514df09adbc7de9b717e676
Andrew Hennessy <andy@yetanotherwebsite.net>
2014-11-16 16:00:46 -0500

    Initial project creation in Atmel Studio

---
+++ TurboSantaInterface.c
@@ -0,0 +1,24 @@
+/*
+ * TurboSantaInterface.c
+ *
+ * Created: 16 Nov 2014 3:55:30 PM
+ *  Author: Andrew
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
